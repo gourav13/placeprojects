d030ced18166decb4d78ae6d67c94d9d1ac35cf1
gourav goyal <gourav13041997@gmail.com>
2018-05-12 00:06:33 +0530

    database facility try

--- server.js
+++ server.js
@@ -1,6 +1,24 @@
+const cool = require('cool-ascii-faces')
 const express=require('express')
 const app=express();
 const path = require('path')
+const { Pool } = require('pg');
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: true
+});
+
+app.get('/db', async (req, res) => {
+    try {
+        const client = await pool.connect()
+    const result = await client.query('SELECT * FROM test_table');
+res.render('pages/db', result);
+client.release();
+} catch (err) {
+    console.error(err);
+    res.send("Error " + err);
+}
+});
 //const hbs=require('express-hbs')
 var a=[1,2,3]
 // app.engine('hbs',hbs.express4
@@ -25,6 +43,7 @@ app.use('/signup',require('./routes/signup.js'))
 app.use('/addproducts',require('./routes/addproducts.js'))
 app.use('/products',require('./routes/products.js'))
 app.use('/upload',require('./routes/upload.js'))
+    .get('/cool', (req, res) => res.send(cool()))
 app.listen(app.get('port'),()=>{
     console.log('server is listening at port 3030')
 })

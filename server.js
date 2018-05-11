96444aa7b1bbb71844bf0b0ccf3c88215c93a4cf
gourav goyal <gourav13041997@gmail.com>
2018-05-12 01:38:14 +0530

    dddd

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const pool = new Pool({
 app.get('/db', async (req, res) => {
     try {
         const client = await pool.connect()
-    const result = await client.query('SELECT * FROM test_table');
+    const result = await client.query('SELECT name FROM test_table');
 res.send(result);
 client.release();
 } catch (err) {
